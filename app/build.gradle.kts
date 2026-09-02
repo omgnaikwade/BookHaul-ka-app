cca60725fc91703fe8f921745ab20d73bde0b3dc
omgnaikwade <omg.naikwade@gmail.com>
2026-09-02 12:04:26 +0530

    Remove debug signing configuration
    Removed debug signing configuration from build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,12 +34,6 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
-    }
   }
 
   buildTypes {
@@ -49,7 +43,6 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
